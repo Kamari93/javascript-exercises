8645d56c77d9176a3603a98fa7bbdd92751c4de0
Kamari <kmoore971@gmail.com>
2023-07-03 09:36:00 -0500

    Add solution to reverseString.js and ran all test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let reverse = ""
+    if (string.length === 0) {
+        return reverse;
+    } else if (string.length >= 0) {
+        for (i = string.length - 1; i >= 0; i--) {
+            reverse += string[i];
+        }
+        return reverse;
+    }
 };
 
 // Do not edit below this line
