0aa3b378f24ff4af4c9fa9909d25a988b8f119ba
Kamari <kmoore971@gmail.com>
2023-07-03 09:02:50 -0500

    Add solution and enabled all test for repeatString excercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let stringRepeat = '';
 
+    if (num < 0) {
+        return 'ERROR';
+    } else if (num === 0) {
+        return stringRepeat;
+    } else {
+        for (let i = 1; i <= num; i++) {
+            stringRepeat += string;
+        }
+        return stringRepeat;
+    }
 };
 
 // Do not edit below this line
