310830abebd6f82a00ef20a39928f5e8d9d35e60
Kamari <kmoore971@gmail.com>
2023-07-04 09:31:34 -0500

    Add solution to sumAll and passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+
+    if (typeof (min) !== "number" || typeof (max) !== "number") {
+        return "ERROR";
+    } else if (min < 0 || max < 0) {
+        return "ERROR";
+    } else {
+        // if the larger value is inputted first switch the vals
+        if (min > max) {
+            [min, max] = [max, min];
+        };
+        // create a var that takes in all vals
+        let total = 0;
+        // loop through a range starting from min and ending at max 
+        for (let i = min; i < max + 1; i++) {
+            total += i;
+        };
+        return total;
+    }
 
 };
 
