2060c7f98e53ee9abb51cfac877e9d7a5021405d
Kamari <kmoore971@gmail.com>
2023-07-04 12:36:32 -0500

    Add solutions to tempConversion and passed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahr) {
+  let cels = (fahr - 32.0) * 5.0 / 9.0;
+  return parseFloat(cels.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cels) {
+  let fahr = (cels * 9.0 / 5.0) + 32.0;
+  return parseFloat(fahr.toFixed(1));
 };
 
 // Do not edit below this line
