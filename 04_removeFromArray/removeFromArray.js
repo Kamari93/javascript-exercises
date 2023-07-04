b6d20097f5cf8eb59e6673a9b1fe2d1b8a3af40c
Kamari <kmoore971@gmail.com>
2023-07-04 08:36:27 -0500

    Add solutions to removefromArray and passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
+// 1st solution with filter function
+const removeFromArray = function (arr, ...removeItems) {
+    // create a delete arr from the 2nd params
+    let deleteItems = Array.from(removeItems, item => item);
+    // create a new arr that only includes items not in deleted arr
+    let filtered = arr.filter(x => !deleteItems.includes(x));
 
+    return filtered;
 };
+
+// 2nd solution with for loop
+// const removeFromArray = function (arr, ...removeItems) {
+//     let removeArr = Array.from(removeItems);
+//     let unique = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (!removeArr.includes(arr[i])) {
+//             unique.push(arr[i]);
+//         }
+//     }
+//     return unique;
+// }
 
 // Do not edit below this line
 module.exports = removeFromArray;
