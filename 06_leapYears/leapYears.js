60077919bc64290c86da3d27a9fbdb0fd077f22d
Kamari <kmoore971@gmail.com>
2023-07-04 10:53:54 -0500

    Add solutions to leapYear and passed all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,26 @@
-const leapYears = function() {
 
+// first solution
+const leapYears = function (year) {
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 };
+
+// Second solution
+// exclude multipes of 100
+// const leapYears = function (year) {
+//     return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+// }
 
 // Do not edit below this line
 module.exports = leapYears;
