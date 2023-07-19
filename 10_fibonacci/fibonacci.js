f87e38998097ddace307afaee6455a8a8533e891
Kamari <kmoore971@gmail.com>
2023-07-19 08:19:42 -0500

    Add fibonacci solutions and passed all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+    // 1, 1, 2, 3, 5, 8, etc.
+    // Fn = Fn-1 + Fn-2
+    // the first 2 indexes of fib are always 1
+    if (n < 0) { return 'OOPS' };
+    if (typeof n === "string") { parseInt(n) };
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= n; i++) {
+        let c = a + b;
+        // shift to the right for next calculation..remeber ab -> c, ab -> c
+        a = b;
+        b = c;
+    }
+    return b;
 };
 
 // Do not edit below this line
