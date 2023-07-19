e7ec8c8bf923adb7cad8e6aa86b7d991f3fba62e
Kamari <kmoore971@gmail.com>
2023-07-19 09:52:32 -0500

    Add findOldest solution and passed all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+    let currentYear = new Date().getFullYear();
+    let oldest = arr.map((person) => {
+        if ('yearOfDeath' in person) {
+            person.life = person.yearOfDeath - person.yearOfBirth;
+            return person;
+        };
+        person.life = currentYear - person.yearOfBirth;
+        return person;
+    }).sort((a, b) => a.life > b.life ? -1 : 1)
 
+    return oldest[0];
 };
 
 // Do not edit below this line
